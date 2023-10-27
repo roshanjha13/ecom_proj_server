3e43b8f2cec8789e19ac7632017e67e1875278b1
Roshan <roshan.jha@hih7.in>
2023-10-27 15:12:55 +0530

    user created and hashed password also

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,37 @@
 const { validationResult } = require("express-validator");
 
+const UserModel = require("../models/userModel");
+const { hashedPassword } = require("../services/authServices");
+
 exports.register = async (req, res) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    console.log(errors.array());
+  if (errors.isEmpty()) {
+    const { name, email, password } = req.body;
+
+    try {
+      const emailExist = await UserModel.findOne({ email });
+
+      if (!emailExist) {
+        const hashed = await hashedPassword(password);
+        const user = await UserModel.create({
+          name,
+          email,
+          password: hashed,
+        });
+        res.status(201).json({
+          data: user,
+          message: `${name} your account created successfully`,
+        });
+      } else {
+        res
+          .status(401)
+          .json({ errors: [{ message: "email already register" }] });
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json("Server Internal error");
+    }
   } else {
-    console.log(req.body);
+    res.status(400).json({ errors: errors.array() });
   }
 };
